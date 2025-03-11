fa49398bf5bdccf340870147f90f0d4458be35c4
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 08:12:23 +0700

    menambahkan class FanLaptop

---
+++ FanLaptop.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022330059
+{
+    internal class FanLaptop
+    {
+        enum State { Quiet, Balanced, Performance, Turbo }
+        private State currentState;
+        public FanLaptop()
+        {
+            currentState = State.Quiet; 
+        }
+        public void ModeDown()
+        {
+            if (currentState == State.Turbo)
+            {
+                currentState = State.Performance;
+                Console.WriteLine($"Fan Turbo berubah menjadi {currentState}");
+            }
+            else if (currentState == State.Performance)
+            {
+                currentState = State.Balanced;
+                Console.WriteLine($"Fan Permonced berubah menjadi {currentState}");
+            }
+            else if (currentState == State.Balanced)
+            {
+                currentState = State.Quiet;
+                Console.WriteLine($"Fan Balanced berubah menjadi {currentState}");
+            }          
+        }
+        public void ModeUp()
+        {
+            if (currentState == State.Quiet)
+            {
+                currentState = State.Balanced;
+                Console.WriteLine($"Fan Quiet berubah menjadi {currentState}");
+            }
+            else if (currentState == State.Balanced)
+            {
+                currentState = State.Performance;
+                Console.WriteLine($"Fan Balanced berubah menjadi {currentState}");
+            }
+            else if (currentState == State.Performance)
+            {
+                currentState = State.Turbo;
+                Console.WriteLine($"Fan Perfomance berubah menjadi {currentState}");
+            }
+        }
+        
+        public void TurboShortcut()
+        {
+            if (currentState == State.Quiet)
+            {
+                currentState = State.Turbo;
+                Console.WriteLine($"Fan Quiet berubah menjadi {currentState}");
+            }
+            else if (currentState == State.Turbo)
+            {
+                currentState = State.Quiet;
+                Console.WriteLine($"Fan Turbo berubah menjadi {currentState}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul4_103022330059;
+class Program
+{
+    static void Main()
+    {
+        KodeProduk.TampilkanDaftarKodeProduk();
+
+        Console.WriteLine("===== Pencarian Kode Produk =====");
+        Console.Write("Masukkan nama produk: ");
+        string produkElektronik = Console.ReadLine();
+        Console.WriteLine($"Kode Produk {produkElektronik}: {KodeProduk.GetKodeProduk(produkElektronik)}\n");
+
+        Console.WriteLine("===== Mode Fan =====");
+        FanLaptop mode = new FanLaptop();
+        while (true)
+        {
+            Console.WriteLine("\nKetik 'up' untuk menaikkan mode atau 'down' untuk menurunkan mode atau 'turbo' untuk mode Turbo (ketik 'exit' untuk keluar): ");
+            string command = Console.ReadLine()?.ToLower();
+
+            if (command == "up")
+                mode.ModeUp();
+            else if (command == "down")
+                mode.ModeDown();
+            else if (command == "turbo")
+                mode.TurboShortcut();
+            else if (command == "exit")
+                break;
+            else
+                Console.WriteLine("Perintah tidak dikenali.");
+        }
+    }
+}
